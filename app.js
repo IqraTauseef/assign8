1a7cbf8b55c395acb439f9ebeff4c7d352578de7
IqraTauseef <156837674+IqraTauseef@users.noreply.github.com>
2024-05-18 23:48:53 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,184 @@
+// ALERT 
+// Question:01
+// let welcome=("HELLO WORLD")
+// alert("Welcome to our website! We're glad you're here.");
+// alert(welcome)
+
+// 2. Write a script to display following message on your web page
+// let wrong=('Error! please enter valid password')
+// alert(wrong)
+
+// 3. Write a script to display following message on your web
+// page: (Hint : Use line break)
+// let line=('Welcome to JS Land \n Happy Coding!')
+// alert(line)
+
+// 4. Write a script to display following messages in sequence:
+// let land=('Welcome to JS Land')
+// alert(land)
+// let tick = '⬛Prevent this page from creating additional dialogs.';
+// alert(tick)
+
+// 5. Generate the following message through browser’s developer console:
+// let run = ('Hello... I can run my web browser console');
+// console.log(run);
+
+// .....................Chapter#02.....................
+// 1. Declare a variable called username.
+// let usernam;
+
+// 2. Declare a variable called myName & assign to it a string that represents your Full Name.
+// let myName='Iqra Tauseef'
+
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+
+// let message;
+// message = "Hello World";
+// alert(message)
+
+// 4. Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+
+// var studentName = "Iqra Tauseef";
+// var studentAge = 25;
+// var course = "Web Development";
+
+// alert("Student Name: " + studentName);
+// alert("Student Age: " + studentAge);
+// alert("Course enrolled: " + course);
+
+// 5. Write a script to display the following alert using one JS variable:
+// alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
+
+// 6. Declare a variable called email and assign to it a string that represents your Email Address(e.g. example@example.com).Show the blow mentioned message in an alert box.(Hint: use string concatenation)
+// let email = "example@example.com";
+// alert("My email address is: " + email);
+
+// 7. Declare a variable called book & give it the value “A smarter way to learn JavaScript”. Display the following message in an alert box:
+// let A='I am trying to learn from the Book A smarter\nway to leran JavaScript'
+// alert(A)
+
+// 8. Write a script to display this in browser through JS
+// document.write('Yah!I can write HTML content through JavaScript')
+
+// 9. Store following string in a variable and show in alert and browser through JS
+// let symbol='▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬'
+// alert(symbol)
+
+// .................Chapter#03.........................
+// 1. Declare a variable called age & assign to it your age. Show your age in an alert box.
+// let age = 25;
+// alert('I am'+ age+'years old')
+
+// Declare & initialize a variable to keep track of how many times a visitor has visited a web page. Show his/her number of visits on your web page. For example: “You have visited this site N times”.
+// let track='You have visited this site 14 times'
+// alert(track)
+
+// 3. Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser:
+// document.write('My birth year is 1998<br>Data type of my decleared variable is number')
+
+// 4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+// let visitor = prompt('Enter your name')
+// let product= prompt('Enter your product')
+// let qunatity=prompt('Enter your quantity')
+
+// document.write(visitor+product+qunatity +'<br>ordered 5 T-shirt(s) on my Clothing store</br>')
+
+// .................chapter#4............
+
+//  1. Declare 3 variables in one statement.
+// let myName='IQRA\n'
+// let religion='ISLAM\n'
+// let nationality='PAKISTANI'
+// alert('My Name is '+myName+'My Religion'+religion+'My Nationality'+nationality)
+
+
+// 2. Declare 5 legal & 5 illegal variable names.
+
+// Legal Variable Names
+// 1.firstName
+// 2._count
+// 3.totalAmount
+// 4.user2
+// 5.$price
+
+// Illegal Variable Names
+// 1.2ndUser (cannot start with a digit)
+// 2.user-name (hyphens are not allowed)
+// 3.var (reserved keyword)
+// 4.total amount (spaces are not allowed)
+// 5.@home (special characters other than $ and _ are not allowed)
+
+
+
+// 3. Display this in your browser
+// document.write("<h1>A heading stating “Rules for naming JS variables” </h1>")
+// document.write("<p>Variable names can only contain letters, digits, underscores (_), and dollar signs ($). For example $my_1stVariable</p>");
+// document.write("<p>Variables must begin with a letter, underscore (_), or dollar sign ($). For example $name, _name, or name</p>");
+// document.write("<p>Variable names are case-sensitive</p>");
+// document.write("<p>Variable names should not be JS keywords</p>");
+
+
+
+//MATH EXPRESSIONS chapter 5
+// let num1 = prompt('Enter number 1: ');
+// let num2 = prompt('Enter number 2: ');
+
+// num1 = parseInt(num1);
+// num2 = parseInt(num2);
+
+// alert(num1+num2)
+// alert(num1-num2)
+// alert(num1*num2)
+// alert(num1/num2)
+// alert(num1%num2)
+
+// document.write('<h1>Table of 4</h1>')
+// document.writeln('4 x 1 = ', 4*1, '<br>')
+// document.write('4 x 2 = ', 4*2, '<br>')
+// document.write('4 x 3 = ', 4*3, '<br>')
+// document.writeln('4 x4 = ', 4*4, '<br>')
+// document.write('4 x 5 = ', 4*5, '<br>')
+// document.write('4 x 6 = ', 4*6, '<br>')
+// document.write('4 x 7 = ', 4*7, '<br>')
+// document.write('4 x 8 = ', 4*8, '<br>')
+// document.write('4 x 9 = ', 4*9, '<br>')
+// document.write('4 x 10 = ', 4*10, '<br>')
+
+// .............Chapter6-9...............
+// let num = 6;
+// // alert(++num);
+// // alert(num);
+// // alert(1 + 3 * 4);
+// // alert(num);
+
+// let input = prompt('Enter Your Name:');
+
+// alert('Welocme to website:'+input)
+
+// 5. Write a program to take input a number from user & display it’s multiplication table on your browser. If user does not enter a new number, multiplication table of 5 should be displayed by default.
+
+// let no=prompt('enter any number',0)
+
+// document.write('<h1>Table of 5</h1>')
+// document.writeln('5 x 1 = ', 5*1, '<br>')
+// document.write('5 x 2 = ', 5*2, '<br>')
+// document.write('5 x 3 = ', 5*3, '<br>')
+// document.writeln('5 x4 = ', 5*4, '<br>')
+// document.write('5 x 5 = ', 5*5, '<br>')
+// document.write('5 x 6 = ', 5*6, '<br>')
+// document.write('5x 7 = ', 5*7, '<br>')
+// document.write('5 x 8 = ', 5*8, '<br>')
+// document.write('5 x 9 = ', 5*9, '<br>')
+// document.write('5 x 10 = ', 5*10, '<br>')
