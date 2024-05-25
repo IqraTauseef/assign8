486d00520eb5e0e6e54e8001a56c021bc5ea9f22
IqraTauseef <156837674+IqraTauseef@users.noreply.github.com>
2024-05-25 23:31:01 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -132,17 +132,82 @@
 
 
 //MATH EXPRESSIONS chapter 5
-// let num1 = prompt('Enter number 1: ');
-// let num2 = prompt('Enter number 2: ');
+// 1. Write a program that take two numbers & add them in a new variable. Show the result in your browser.
+//  let num1 = 3;
+// let num2 = 5;
+// let sum = num1 + num2;
+// document.write("The sum of " + num1 + "and " + num2 + "is " + sum +'<br>');
 
-// num1 = parseInt(num1);
-// num2 = parseInt(num2);
+// 2. Repeat task1 for subtraction, multiplication, division & modulus.
+// let n1 = 3;
+// let n2 = 5;
+// let subtraction = n1 - n2;
+// document.write("The subtraction of " + n1 + "and " + n2 + "is " + subtraction +'<br>');
 
-// alert(num1+num2)
-// alert(num1-num2)
-// alert(num1*num2)
-// alert(num1/num2)
-// alert(num1%num2)
+// let um1 = 3;
+// let um2 = 5;
+// let multiplication = um1 * um2;
+// document.write("The multiplication of " + um1 + "and " + um2 + "is " + multiplication +'<br>');
+
+// let nm1 = 3;
+// let nm2 = 5;
+// let division = nm1 / nm2;
+// document.write("The division of " + nm1 + "and " + nm2 + "is " + division +'<br>');
+
+// let nu1 = 3;
+// let nu2 = 5;
+// let modulus = nu1 % nu2;
+// document.write("The modulus of " + nu1 + "and " + nu2 + "is " + modulus +'<br>');
+
+// 3. Do the following using JS Mathematic Expressions
+// a. Declare a variable
+// let myVariable='WELCOME TO JS LAND'
+
+// // b. Show the value of variable in your browser like “Value after variable declaration is: ??”.
+// console.log("Value after variable declaration is: " + myVariable);
+
+// // c. Initialize the variable with some number.
+
+// myVariable = 5;
+// console.log("Initial value: " + myVariable);
+// // d. Show the value of variable in your browser
+// document.write("Initial value: " + myVariable+'<br>');
+
+// // e. Increment the variable
+// myVariable++;
+
+
+// // f. Show the value of variable in your browser like “Value after increment is: 6”.
+// document.write("Increment: " + myVariable+'<br>');
+
+
+// // g. Add 7 to the variable
+// myVariable += 7;
+
+// // h. Show the value of variable in your browser like “Value after addition is: 13”.
+// document.write("Value after addition is: " + myVariable+'<br>');
+
+// // i. Decrement the variable
+// myVariable--;
+
+// // j. Show the value of variable in your browser
+// document.write("Value after decrement is: " + myVariable+'<br>');
+
+// // k. Show the remainder after dividing the variable's value by 3
+// let reminder = myVariable % 3;
+
+// // l. Output: "The remainder is: 0"
+// document.write("The reminder is: " + reminder+'<br>');
+
+// 4. Cost of one movie ticket is 600 PKR. Write a script to store ticket price in a variable & calculate the cost of buying 5 tickets to a movie. Example output:
+// Store the ticket price in a variable
+// let ticketPrice = 600;
+// let totalCost = ticketPrice * 5;
+// document.write("The cost of buying 5 movie tickets is: " + totalCost + " PKR");
+
+
+// 5. Write a script to display multiplication table of any
+// number in your browser. 
 
 // document.write('<h1>Table of 4</h1>')
 // document.writeln('4 x 1 = ', 4*1, '<br>')
@@ -156,8 +221,134 @@
 // document.write('4 x 9 = ', 4*9, '<br>')
 // document.write('4 x 10 = ', 4*10, '<br>')
 
+
+// 6. The Temperature Converter: It’s hot out! Let’s make a
+// converter based on the steps here.
+// a. Store a Celsius temperature into a variable.
+// b. Convert it to Fahrenheit & output “NNoC is NNoF”.
+// c. Now store a Fahrenheit temperature into a variable.
+// d. Convert it to Celsius & output “NNoF is NNoC”.
+
+// let c=25
+// let f = (c* 9/5) + 32;
+// document.write(c+'<sup>o</sup>C is'+f+'<sup>o</sup>F <br>')
+ 
+// let F=70
+// let C=(F-32)*5/9;
+// document.write(F+'<sup>o</sup>F is'+C+'<sup>o</sup>C')
+
+
+// 7. Write a program to implement checkout process of a shopping cart system for an e-commerce website. Store the following in variables
+
+// let price1 = 650;
+// let quantity1 = 3;
+// let price2 = 100;
+// let quantity2 = 7;
+// let shippingCharges = 100;
+
+// // Compute the total cost
+// let subTotal = (price1 * quantity1) + (price2 * quantity2);
+//  totalCost = subTotal + shippingCharges;
+
+// // Show the output
+// document.write("Shopping Cart <br>");
+// document.write("Price of 1 item is " + price1+'<br>');
+// document.write("Quantity of item 1 is " + quantity1+'<br>');
+// document.write("Price of item 2 is " + price2+'<br>');
+// document.write("Quantity of item 2 is " + quantity2+'<br>');
+// document.write("Shipping charges " + shippingCharges+'<br> <br>');
+// document.write("Total cost of your order is " + totalCost);
+
+ // 8. Store total marks & marks obtained by a student in 2 variables. Compute the percentage & show the result in your browser
+//  let totalMarks = 1100;
+//  let marksObtained = 735;
+ 
+ 
+//  let percentage = (marksObtained / totalMarks) * 100;
+ 
+ 
+//  document.write("Marks Sheet:<br>");
+//  document.write("Total marks: " + totalMarks +'<br>');
+//  document.write("Marks obtained: " + marksObtained+'<br>');
+//  document.write("Percentage: " + percentage + "%" +'<br>');
+// //  9. Assume we have 10 US dollars & 25 Saudi Riyals. Write a script to convert the total currency to Pakistani Rupees. Perform all calculations in a single expression.(Exchange rates : 1 US Dollar = 104.80 Pakistani Rupee and 1 Saudi Riyal = 28 Pakistani Rupee)
+// let totalPKR = (10 * 104.80) + (25 * 28);
+// document.write("Total currency in PKR: " + totalPKR +'<br>');
+
+
+// 10. Write a program to initialize a variable with some number and do arithmetic in following sequence: a. Add 5 b. Multiply by 10 c. Divide the result by 2 Perform all calculations in a single expression
+// let math = (x = 10, x + 5) * 10 / 2;
+// console.log(math);
+
+//Question#11
+// let currentYear = 2016;
+// let birthYear = 1992;
+
+// let age = currentYear - birthYear;
+
+// document.write("Age calculator <br>");
+// document.write("Current age: " + currentYear +'<br>');
+// document.write("Birthyear: " + birthYear +'<br>');
+// document.write("Your age is: " + age +'<br>');
+
+//Question#12
+// let radius = 20;
+// let pi = 3.142;
+
+// let circumference = 2 * pi * radius;
+// let area = pi * radius * radius;
+
+// document.write("The Geometrizer <br>");
+// document.write("Radius of circle: " + radius +'<br>');
+// document.write("The circumference is " + circumference+'<br>');
+// document.write("The area is " + area+'<br>');
+
+//Question13
+// let favoriteSnack = "chocolate chips";
+// let currentAge = 15;
+// let maxAge = 65;
+// let snacksperday = 3;
+
+// let totalSnacks = (maxAge - currentAge) * 365 * snacksperday;
+
+// document.write("The Lifetime Supply Calculator <br>");
+// document.write("Favourite snack: " + favoriteSnack+'<br>');
+// document.write("Current age: " + currentAge+'<br>');
+// document.write("Estimated maximum age: " + maxAge+'<br>');
+// document.write("Amount of snacks per day: " + snacksperday+'<br>');
+// document.write("You will need " + totalSnacks + " " + favoriteSnack + " to last you until the ripe old age of " + maxAge+'<br>');
+
+
 // .............Chapter6-9...............
-// let num = 6;
+//Question#1
+// let a = 10;
+
+// document.write("Result: The value of a is:", a+'<br><br>');
+// document.write("The value of ++a is:", ++a+'<br>');
+// document.write("Now the value of a is:", a+'<br><br>');
+// document.write("The value of ++a is:", ++a+'<br>');
+// document.write("Now the value of a is:", a+'<br><br>');
+// document.write("The value of --a is:", --a+'<br>');
+// document.write("Now the value of a is:", a+'<br><br>');
+// document.write("The value of a-- is:", a--+'<br>');
+// document.write("Now the value of a is:", a+'<br><br>');
+
+//Question#2
+// let A = 2, b = 1;
+
+// --A;
+// let stage2 = --A - --b;
+// let stage3 = --A - --b + ++b;
+// let result = --A - --b + ++b + b--;
+
+// // Output
+// document.write("Value of A after Stage 1:", A+'<br>');
+// document.write("Value of b after Stage 1:", b+'<br>');
+// document.write("Result after Stage 2:", stage2+'<br>');
+// document.write("Result after Stage 3:", stage3+'<br>');
+// document.write("Result after Stage 4:", result+'<br>');
+// Question#5
+
 // // alert(++num);
 // // alert(num);
 // // alert(1 + 3 * 4);
@@ -182,3 +373,31 @@
 // document.write('5 x 8 = ', 5*8, '<br>')
 // document.write('5 x 9 = ', 5*9, '<br>')
 // document.write('5 x 10 = ', 5*10, '<br>')
+// //Question#6
+// // a) Take three subjects' names from the user and store them in 3 different variables.
+// let subject1 = prompt("Enter the first subject name:");
+// let subject2 = prompt("Enter the second subject name:");
+// let subject3 = prompt("Enter the third subject name:");
+// // b) Total marks for each subject is 100, store it in another variable.
+// const totalMarks = 100;
+
+// // c) Take obtained marks for the first subject from the user and store it in a different variable.
+// let obtainedMarks1 = parseInt(prompt("Enter obtained marks for " + subject1 + ":"));
+
+// // d) Take obtained marks for the remaining 2 subjects from the user and store them in variables.
+// let obtainedMarks2 = parseInt(prompt("Enter obtained marks for " + subject2 + ":"));
+// let obtainedMarks3 = parseInt(prompt("Enter obtained marks for " + subject3 + ":"));
+
+// // e) Calculate total marks and percentage.
+// let totalObtainedMarks = obtainedMarks1 + obtainedMarks2 + obtainedMarks3;
+// let percentage1 = (obtainedMarks1 / totalMarks) * 100;
+// let percentage2 = (obtainedMarks2 / totalMarks) * 100;
+// let percentage3 = (obtainedMarks3 / totalMarks) * 100;
+// let overallPercentage = (totalObtainedMarks / (totalMarks * 3)) * 100;
+
+// // Create a table to display the result
+// let table = "<table border='1'><tr><th>Subject</th><th>Total Marks</th><th>Obtained Marks</th><th>Percentage</th></tr>";
+// document.write (table += "<tr><td>" + subject1 + "</td><td>" + totalMarks + "</td><td>" + obtainedMarks1 + "</td><td>" + percentage1 + "%</td></tr>");
+// document.write(table += "<tr><td>" + subject2 + "</td><td>" + totalMarks + "</td><td>" + obtainedMarks2 + "</td><td>" + percentage2+ "%</td></tr>");
+// document.write (table += "<tr><td>" + subject3 + "</td><td>" + totalMarks + "</td><td>" + obtainedMarks3 + "</td><td>" + percentage3 + "%</td></tr>");
+// document.write (table += "<tr><th>Total</th><th>" + (totalMarks * 3) + "</th><th>" + totalObtainedMarks + "</th><th>" + overallPercentage + "%</th></tr></table>");
