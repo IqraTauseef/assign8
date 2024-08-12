b0afd46ba2a246d76f23bb84eb8f73fd271bf251
IqraTauseef <156837674+IqraTauseef@users.noreply.github.com>
2024-08-12 23:09:08 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,403 +1,68 @@
 // ALERT 
 // Question:01
-// let welcome=("HELLO WORLD")
-// alert("Welcome to our website! We're glad you're here.");
-// alert(welcome)
+let welcome=("HELLO WORLD")
+alert("Welcome to our website! We're glad you're here.");
+alert(welcome)
 
 // 2. Write a script to display following message on your web page
-// let wrong=('Error! please enter valid password')
-// alert(wrong)
+let wrong=('Error! please enter valid password')
+alert(wrong)
 
-// 3. Write a script to display following message on your web
-// page: (Hint : Use line break)
-// let line=('Welcome to JS Land \n Happy Coding!')
-// alert(line)
+// 3. Write a script to display following message on your webpage: (Hint : Use line break)
+let line=('Welcome to JS Land \n Happy Coding!')
+alert(line)
 
 // 4. Write a script to display following messages in sequence:
-// let land=('Welcome to JS Land')
-// alert(land)
-// let tick = '⬛Prevent this page from creating additional dialogs.';
-// alert(tick)
+let land=('Welcome to JS Land')
+alert(land)
+let tick = '⬛Prevent this page from creating additional dialogs.';
+alert(tick)
 
 // 5. Generate the following message through browser’s developer console:
-// let run = ('Hello... I can run my web browser console');
-// console.log(run);
+let run = ('Hello... I can run my web browser console');
+console.log(run);
 
 // .....................Chapter#02.....................
 // 1. Declare a variable called username.
-// let usernam;
+let usernam;
 
 // 2. Declare a variable called myName & assign to it a string that represents your Full Name.
-// let myName='Iqra Tauseef'
+let myName='Iqra Tauseef'
 
 // 3. Write script to
 // a) Declare a JS variable, titled message.
 // b) Assign “Hello World” to variable message
 // c) Display the message in alert box.
-
-// let message;
-// message = "Hello World";
-// alert(message)
+let message;
+message = "Hello World";
+alert(message)
 
 // 4. Write a script to save student’s bio data in JS variables and show the data in alert boxes.
 
-// var studentName = "Iqra Tauseef";
-// var studentAge = 25;
-// var course = "Web Development";
+var studentName = "Iqra Tauseef";
+var studentAge = 25;
+var course = "Web Development";
 
-// alert("Student Name: " + studentName);
-// alert("Student Age: " + studentAge);
-// alert("Course enrolled: " + course);
+alert("Student Name: " + studentName);
+alert("Student Age: " + studentAge);
+alert("Course enrolled: " + course);
 
 // 5. Write a script to display the following alert using one JS variable:
-// alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
+alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
 
 // 6. Declare a variable called email and assign to it a string that represents your Email Address(e.g. example@example.com).Show the blow mentioned message in an alert box.(Hint: use string concatenation)
 // let email = "example@example.com";
-// alert("My email address is: " + email);
+alert("My email address is: " + email);
 
 // 7. Declare a variable called book & give it the value “A smarter way to learn JavaScript”. Display the following message in an alert box:
-// let A='I am trying to learn from the Book A smarter\nway to leran JavaScript'
-// alert(A)
+let Ab='I am trying to learn from the Book A smarter\nway to leran JavaScript'
+alert(Ab)
 
 // 8. Write a script to display this in browser through JS
-// document.write('Yah!I can write HTML content through JavaScript')
+document.write('Yah!I can write HTML content through JavaScript')
 
 // 9. Store following string in a variable and show in alert and browser through JS
 // let symbol='▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬'
-// alert(symbol)
+alert(symbol)
 
-// .................Chapter#03.........................
-// 1. Declare a variable called age & assign to it your age. Show your age in an alert box.
-// let age = 25;
-// alert('I am'+ age+'years old')
 
-// Declare & initialize a variable to keep track of how many times a visitor has visited a web page. Show his/her number of visits on your web page. For example: “You have visited this site N times”.
-// let track='You have visited this site 14 times'
-// alert(track)
-
-// 3. Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser:
-// document.write('My birth year is 1998<br>Data type of my decleared variable is number')
-
-// 4. A visitor visits an online clothing store
-// www.xyzClothing.com . Write a script to store in variables
-// the following information:
-// a. Visitor’s name
-// b. Product title
-// c. Quantity i.e. how many products a visitor wants to
-// order
-// Show the following message in your browser: “John
-// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
-
-// let visitor = prompt('Enter your name')
-// let product= prompt('Enter your product')
-// let qunatity=prompt('Enter your quantity')
-
-// document.write(visitor+product+qunatity +'<br>ordered 5 T-shirt(s) on my Clothing store</br>')
-
-// .................chapter#4............
-
-//  1. Declare 3 variables in one statement.
-// let myName='IQRA\n'
-// let religion='ISLAM\n'
-// let nationality='PAKISTANI'
-// alert('My Name is '+myName+'My Religion'+religion+'My Nationality'+nationality)
-
-
-// 2. Declare 5 legal & 5 illegal variable names.
-
-// Legal Variable Names
-// 1.firstName
-// 2._count
-// 3.totalAmount
-// 4.user2
-// 5.$price
-
-// Illegal Variable Names
-// 1.2ndUser (cannot start with a digit)
-// 2.user-name (hyphens are not allowed)
-// 3.var (reserved keyword)
-// 4.total amount (spaces are not allowed)
-// 5.@home (special characters other than $ and _ are not allowed)
-
-
-
-// 3. Display this in your browser
-// document.write("<h1>A heading stating “Rules for naming JS variables” </h1>")
-// document.write("<p>Variable names can only contain letters, digits, underscores (_), and dollar signs ($). For example $my_1stVariable</p>");
-// document.write("<p>Variables must begin with a letter, underscore (_), or dollar sign ($). For example $name, _name, or name</p>");
-// document.write("<p>Variable names are case-sensitive</p>");
-// document.write("<p>Variable names should not be JS keywords</p>");
-
-
-
-//MATH EXPRESSIONS chapter 5
-// 1. Write a program that take two numbers & add them in a new variable. Show the result in your browser.
-//  let num1 = 3;
-// let num2 = 5;
-// let sum = num1 + num2;
-// document.write("The sum of " + num1 + "and " + num2 + "is " + sum +'<br>');
-
-// 2. Repeat task1 for subtraction, multiplication, division & modulus.
-// let n1 = 3;
-// let n2 = 5;
-// let subtraction = n1 - n2;
-// document.write("The subtraction of " + n1 + "and " + n2 + "is " + subtraction +'<br>');
-
-// let um1 = 3;
-// let um2 = 5;
-// let multiplication = um1 * um2;
-// document.write("The multiplication of " + um1 + "and " + um2 + "is " + multiplication +'<br>');
-
-// let nm1 = 3;
-// let nm2 = 5;
-// let division = nm1 / nm2;
-// document.write("The division of " + nm1 + "and " + nm2 + "is " + division +'<br>');
-
-// let nu1 = 3;
-// let nu2 = 5;
-// let modulus = nu1 % nu2;
-// document.write("The modulus of " + nu1 + "and " + nu2 + "is " + modulus +'<br>');
-
-// 3. Do the following using JS Mathematic Expressions
-// a. Declare a variable
-// let myVariable='WELCOME TO JS LAND'
-
-// // b. Show the value of variable in your browser like “Value after variable declaration is: ??”.
-// console.log("Value after variable declaration is: " + myVariable);
-
-// // c. Initialize the variable with some number.
-
-// myVariable = 5;
-// console.log("Initial value: " + myVariable);
-// // d. Show the value of variable in your browser
-// document.write("Initial value: " + myVariable+'<br>');
-
-// // e. Increment the variable
-// myVariable++;
-
-
-// // f. Show the value of variable in your browser like “Value after increment is: 6”.
-// document.write("Increment: " + myVariable+'<br>');
-
-
-// // g. Add 7 to the variable
-// myVariable += 7;
-
-// // h. Show the value of variable in your browser like “Value after addition is: 13”.
-// document.write("Value after addition is: " + myVariable+'<br>');
-
-// // i. Decrement the variable
-// myVariable--;
-
-// // j. Show the value of variable in your browser
-// document.write("Value after decrement is: " + myVariable+'<br>');
-
-// // k. Show the remainder after dividing the variable's value by 3
-// let reminder = myVariable % 3;
-
-// // l. Output: "The remainder is: 0"
-// document.write("The reminder is: " + reminder+'<br>');
-
-// 4. Cost of one movie ticket is 600 PKR. Write a script to store ticket price in a variable & calculate the cost of buying 5 tickets to a movie. Example output:
-// Store the ticket price in a variable
-// let ticketPrice = 600;
-// let totalCost = ticketPrice * 5;
-// document.write("The cost of buying 5 movie tickets is: " + totalCost + " PKR");
-
-
-// 5. Write a script to display multiplication table of any
-// number in your browser. 
-
-// document.write('<h1>Table of 4</h1>')
-// document.writeln('4 x 1 = ', 4*1, '<br>')
-// document.write('4 x 2 = ', 4*2, '<br>')
-// document.write('4 x 3 = ', 4*3, '<br>')
-// document.writeln('4 x4 = ', 4*4, '<br>')
-// document.write('4 x 5 = ', 4*5, '<br>')
-// document.write('4 x 6 = ', 4*6, '<br>')
-// document.write('4 x 7 = ', 4*7, '<br>')
-// document.write('4 x 8 = ', 4*8, '<br>')
-// document.write('4 x 9 = ', 4*9, '<br>')
-// document.write('4 x 10 = ', 4*10, '<br>')
-
-
-// 6. The Temperature Converter: It’s hot out! Let’s make a
-// converter based on the steps here.
-// a. Store a Celsius temperature into a variable.
-// b. Convert it to Fahrenheit & output “NNoC is NNoF”.
-// c. Now store a Fahrenheit temperature into a variable.
-// d. Convert it to Celsius & output “NNoF is NNoC”.
-
-// let c=25
-// let f = (c* 9/5) + 32;
-// document.write(c+'<sup>o</sup>C is'+f+'<sup>o</sup>F <br>')
- 
-// let F=70
-// let C=(F-32)*5/9;
-// document.write(F+'<sup>o</sup>F is'+C+'<sup>o</sup>C')
-
-
-// 7. Write a program to implement checkout process of a shopping cart system for an e-commerce website. Store the following in variables
-
-// let price1 = 650;
-// let quantity1 = 3;
-// let price2 = 100;
-// let quantity2 = 7;
-// let shippingCharges = 100;
-
-// // Compute the total cost
-// let subTotal = (price1 * quantity1) + (price2 * quantity2);
-//  totalCost = subTotal + shippingCharges;
-
-// // Show the output
-// document.write("Shopping Cart <br>");
-// document.write("Price of 1 item is " + price1+'<br>');
-// document.write("Quantity of item 1 is " + quantity1+'<br>');
-// document.write("Price of item 2 is " + price2+'<br>');
-// document.write("Quantity of item 2 is " + quantity2+'<br>');
-// document.write("Shipping charges " + shippingCharges+'<br> <br>');
-// document.write("Total cost of your order is " + totalCost);
-
- // 8. Store total marks & marks obtained by a student in 2 variables. Compute the percentage & show the result in your browser
-//  let totalMarks = 1100;
-//  let marksObtained = 735;
- 
- 
-//  let percentage = (marksObtained / totalMarks) * 100;
- 
- 
-//  document.write("Marks Sheet:<br>");
-//  document.write("Total marks: " + totalMarks +'<br>');
-//  document.write("Marks obtained: " + marksObtained+'<br>');
-//  document.write("Percentage: " + percentage + "%" +'<br>');
-// //  9. Assume we have 10 US dollars & 25 Saudi Riyals. Write a script to convert the total currency to Pakistani Rupees. Perform all calculations in a single expression.(Exchange rates : 1 US Dollar = 104.80 Pakistani Rupee and 1 Saudi Riyal = 28 Pakistani Rupee)
-// let totalPKR = (10 * 104.80) + (25 * 28);
-// document.write("Total currency in PKR: " + totalPKR +'<br>');
-
-
-// 10. Write a program to initialize a variable with some number and do arithmetic in following sequence: a. Add 5 b. Multiply by 10 c. Divide the result by 2 Perform all calculations in a single expression
-// let math = (x = 10, x + 5) * 10 / 2;
-// console.log(math);
-
-//Question#11
-// let currentYear = 2016;
-// let birthYear = 1992;
-
-// let age = currentYear - birthYear;
-
-// document.write("Age calculator <br>");
-// document.write("Current age: " + currentYear +'<br>');
-// document.write("Birthyear: " + birthYear +'<br>');
-// document.write("Your age is: " + age +'<br>');
-
-//Question#12
-// let radius = 20;
-// let pi = 3.142;
-
-// let circumference = 2 * pi * radius;
-// let area = pi * radius * radius;
-
-// document.write("The Geometrizer <br>");
-// document.write("Radius of circle: " + radius +'<br>');
-// document.write("The circumference is " + circumference+'<br>');
-// document.write("The area is " + area+'<br>');
-
-//Question13
-// let favoriteSnack = "chocolate chips";
-// let currentAge = 15;
-// let maxAge = 65;
-// let snacksperday = 3;
-
-// let totalSnacks = (maxAge - currentAge) * 365 * snacksperday;
-
-// document.write("The Lifetime Supply Calculator <br>");
-// document.write("Favourite snack: " + favoriteSnack+'<br>');
-// document.write("Current age: " + currentAge+'<br>');
-// document.write("Estimated maximum age: " + maxAge+'<br>');
-// document.write("Amount of snacks per day: " + snacksperday+'<br>');
-// document.write("You will need " + totalSnacks + " " + favoriteSnack + " to last you until the ripe old age of " + maxAge+'<br>');
-
-
-// .............Chapter6-9...............
-//Question#1
-// let a = 10;
-
-// document.write("Result: The value of a is:", a+'<br><br>');
-// document.write("The value of ++a is:", ++a+'<br>');
-// document.write("Now the value of a is:", a+'<br><br>');
-// document.write("The value of ++a is:", ++a+'<br>');
-// document.write("Now the value of a is:", a+'<br><br>');
-// document.write("The value of --a is:", --a+'<br>');
-// document.write("Now the value of a is:", a+'<br><br>');
-// document.write("The value of a-- is:", a--+'<br>');
-// document.write("Now the value of a is:", a+'<br><br>');
-
-//Question#2
-// let A = 2, b = 1;
-
-// --A;
-// let stage2 = --A - --b;
-// let stage3 = --A - --b + ++b;
-// let result = --A - --b + ++b + b--;
-
-// // Output
-// document.write("Value of A after Stage 1:", A+'<br>');
-// document.write("Value of b after Stage 1:", b+'<br>');
-// document.write("Result after Stage 2:", stage2+'<br>');
-// document.write("Result after Stage 3:", stage3+'<br>');
-// document.write("Result after Stage 4:", result+'<br>');
-// Question#5
-
-// // alert(++num);
-// // alert(num);
-// // alert(1 + 3 * 4);
-// // alert(num);
-
-// let input = prompt('Enter Your Name:');
-
-// alert('Welocme to website:'+input)
-
-// 5. Write a program to take input a number from user & display it’s multiplication table on your browser. If user does not enter a new number, multiplication table of 5 should be displayed by default.
-
-// let no=prompt('enter any number',0)
-
-// document.write('<h1>Table of 5</h1>')
-// document.writeln('5 x 1 = ', 5*1, '<br>')
-// document.write('5 x 2 = ', 5*2, '<br>')
-// document.write('5 x 3 = ', 5*3, '<br>')
-// document.writeln('5 x4 = ', 5*4, '<br>')
-// document.write('5 x 5 = ', 5*5, '<br>')
-// document.write('5 x 6 = ', 5*6, '<br>')
-// document.write('5x 7 = ', 5*7, '<br>')
-// document.write('5 x 8 = ', 5*8, '<br>')
-// document.write('5 x 9 = ', 5*9, '<br>')
-// document.write('5 x 10 = ', 5*10, '<br>')
-// //Question#6
-// // a) Take three subjects' names from the user and store them in 3 different variables.
-// let subject1 = prompt("Enter the first subject name:");
-// let subject2 = prompt("Enter the second subject name:");
-// let subject3 = prompt("Enter the third subject name:");
-// // b) Total marks for each subject is 100, store it in another variable.
-// const totalMarks = 100;
-
-// // c) Take obtained marks for the first subject from the user and store it in a different variable.
-// let obtainedMarks1 = parseInt(prompt("Enter obtained marks for " + subject1 + ":"));
-
-// // d) Take obtained marks for the remaining 2 subjects from the user and store them in variables.
-// let obtainedMarks2 = parseInt(prompt("Enter obtained marks for " + subject2 + ":"));
-// let obtainedMarks3 = parseInt(prompt("Enter obtained marks for " + subject3 + ":"));
-
-// // e) Calculate total marks and percentage.
-// let totalObtainedMarks = obtainedMarks1 + obtainedMarks2 + obtainedMarks3;
-// let percentage1 = (obtainedMarks1 / totalMarks) * 100;
-// let percentage2 = (obtainedMarks2 / totalMarks) * 100;
-// let percentage3 = (obtainedMarks3 / totalMarks) * 100;
-// let overallPercentage = (totalObtainedMarks / (totalMarks * 3)) * 100;
-
-// // Create a table to display the result
-// let table = "<table border='1'><tr><th>Subject</th><th>Total Marks</th><th>Obtained Marks</th><th>Percentage</th></tr>";
-// document.write (table += "<tr><td>" + subject1 + "</td><td>" + totalMarks + "</td><td>" + obtainedMarks1 + "</td><td>" + percentage1 + "%</td></tr>");
-// document.write(table += "<tr><td>" + subject2 + "</td><td>" + totalMarks + "</td><td>" + obtainedMarks2 + "</td><td>" + percentage2+ "%</td></tr>");
-// document.write (table += "<tr><td>" + subject3 + "</td><td>" + totalMarks + "</td><td>" + obtainedMarks3 + "</td><td>" + percentage3 + "%</td></tr>");
-// document.write (table += "<tr><th>Total</th><th>" + (totalMarks * 3) + "</th><th>" + totalObtainedMarks + "</th><th>" + overallPercentage + "%</th></tr></table>");
